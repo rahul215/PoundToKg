466d022935e0a22671a41b468b0502257a8c1cf5
rahul215 <rahul.saxena_cs18@gla.ac.in>
2020-01-11 07:24:35 -1200

    Initial commit

---
+++ src/Repeat.java
@@ -0,0 +1,7 @@
+public class Repeat {
+    public static void main(String[] args) {
+        double NoofPound = 200d;
+        double Convertedkilograms = NoofPound * 0.45359237;
+        System.out.println(Convertedkilograms);
+    }
+}
